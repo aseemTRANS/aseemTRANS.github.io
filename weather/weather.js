e6b4b6934c51b555be8492d5b1310bee9f8aa8cc
Aseem Shaikh <157912902+aseemTRANS@users.noreply.github.com>
2025-09-10 21:32:26 +0100

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,12 +1,27 @@
+// 1) Base map
 var map = L.map('map').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var basemap = L.tileLayer(basemapUrl).addTo(map);
+L.tileLayer(basemapUrl).addTo(map);
 
+// 2) Radar (WMS)
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
-var radarDisplayOptions = {
+var radar = L.tileLayer.wms(radarUrl, {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
   transparent: true
-};
-var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
+}).addTo(map);
 
+// 3) Alerts (GeoJSON via NWS API)
+var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
+$.getJSON(weatherAlertsUrl, function(data) {
+  L.geoJSON(data, {
+    style: function(feature) {
+      var c = 'orange';
+      if (feature.properties.severity === 'Severe') c = 'red';
+      return { color: c };
+    },
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(feature.properties.headline);
+    }
+  }).addTo(map);
+});
